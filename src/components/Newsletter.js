2b3e6f916bdf4c1bbb5cedc7884e31bfdb13c040
Sandu1234 <sandunayanathara99@gmail.com>
2024-01-30 21:53:00 +0530

    changes 3

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -35,14 +35,15 @@ return (
       <Container>
         <h2 style={{ textAlign: 'center', fontWeight: 'bold', marginBottom: '20px'}}>My Latest Blogs</h2>
         <Row className="justify-content-md-center">
-          <Col lg={4} md={6} style={{ marginBottom: '20px', paddingLeft: '100px', paddingRight: '15px', paddingTop:'20px' }}>
-            <BlogCard
-              imagePath={yourImagePath1}
-              title="Real-World Applications of Machine Learning"
-              excerpt="Introduction Machine Learning (ML) has emerged as a transformative technology, influencing numerous sectors with its ability to learn from..."
-              readMoreUrl="https://medium.com/@sandunayanathara99/real-world-applications-of-machine-learning-transforming-healthcare-finance-and-automotive-ee87340b36e8"            />
-          </Col>
-          <Col lg={4} md={6} style={{ marginBottom: '20px', paddingLeft: '60px', paddingRight: '15px', paddingTop:'20px'}}>
+  <Col xs={12} md={6} lg={4} style={{ marginBottom: '20px', paddingLeft: '15px', paddingRight: '15px', paddingTop:'20px' }}>
+    <BlogCard
+      imagePath={yourImagePath1}
+      title="Real-World Applications of Machine Learning"
+      excerpt="Introduction Machine Learning (ML) has emerged as a transformative technology..."
+      readMoreUrl="https://medium.com/@sandunayanathara99/real-world-applications-of-machine-learning-transforming-healthcare-finance-and-automotive-ee87340b36e8"
+    />
+  </Col>
+  <Col xs={12} md={6} lg={4} style={{ marginBottom: '20px', paddingLeft: '15px', paddingRight: '15px', paddingTop:'20px' }}>
             <BlogCard
               imagePath={yourImagePath2}
               title="Navigating the Agile Seas"
@@ -50,7 +51,7 @@ return (
               readMoreUrl="https://medium.com/@sandunayanathara99/navigating-the-agile-seas-advanced-it-project-management-techniques-2075f5cdc404"
                         />
           </Col>
-          <Col lg={4} md={6} style={{ marginBottom: '20px', paddingLeft: '15px', paddingRight: '15px', paddingTop:'20px' }}>
+          <Col xs={12} md={6} lg={4} style={{ marginBottom: '20px', paddingLeft: '15px', paddingRight: '15px', paddingTop:'20px' }}>
             <BlogCard
               imagePath={yourImagePath3}
               title="Design Patterns"
