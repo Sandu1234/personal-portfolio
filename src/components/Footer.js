5e2f93273ec0a8483114271b9428171807c56d3c
SanduK99 <sandukumarasinghe99@gmail.com>
2024-10-09 21:15:09 +0530

    New style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 // import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/LOGO2.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon4 from "../assets/img/nav-icon2.svg";
+import navIcon4 from '../assets/img/nacicon4.png';
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
